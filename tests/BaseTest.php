8633386c3df70c3dc5a4bf16086f6e274aeb8429
odin <odinuv@gmail.com>
2021-09-24 20:37:23 +0200

    tests: fix envs

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -12,7 +12,7 @@ abstract class BaseTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
-        $requiredEnvs = ['public_queue_api_url', 'test_storage_api_token'];
+        $requiredEnvs = ['public_queue_api_url', 'test_storage_api_token', 'storage_api_url'];
         foreach ($requiredEnvs as $env) {
             if (empty(getenv($env))) {
                 throw new Exception(sprintf('Environment variable "%s" is empty', $env));
